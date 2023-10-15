08717d816a251267660faaca9824c13281cff02f
Amelya Kartika <126840619+AmelyaKartika@users.noreply.github.com>
2023-10-15 18:52:30 +0700

    list_tile_2201082002
    Praktek Flutter bagian main .dart

---
+++ list_tile/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("List Tile"),
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+              title: Text("Sandhika Rahardi"),
+              subtitle: Text(
+                "This is subtitle okkay..gasafagshsjdhjsksjdlkalsklakslajssas",
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 PM"),
+              // tileColor: Colors.amber,
+              //dense: true,
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Sandhika Rahardi"),
+              subtitle: Text("This is subtitle okkay.."),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Sandhika Rahardi"),
+              subtitle: Text("This is subtitle okkay.."),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Sandhika Rahardi"),
+              subtitle: Text("This is subtitle okkay.."),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Sandhika Rahardi"),
+              subtitle: Text("This is subtitle okkay.."),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 PM"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
