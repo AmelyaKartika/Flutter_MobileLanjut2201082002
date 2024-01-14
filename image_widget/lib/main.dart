d4a4e98f51ee8b3c7129f31c1837dd70b1b6e1b1
Amelya Kartika <126840619+AmelyaKartika@users.noreply.github.com>
2024-01-14 13:18:52 +0700

    image_widget_2201082002
    Project Flutter

---
+++ image_widget/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Image Widget"),
+        ),
+        body: Center(
+          child: Container(
+            width: 350,
+            height: 500,
+            color: Colors.amber,
+            child: Image.network("https://picsum.photos/200/300",
+            ),
+            //child: Image(
+              //fit: BoxFit.cover,
+             // image: AssetImage("images/kawan.jpg"),
+              //image: NetworkImage("https://picsum.photos/200/300"),
+           // ),
+          ),
+        ),
+      ),
+    );
+  }
+}
