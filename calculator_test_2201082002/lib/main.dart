c6f881cf290d9e2a6dafc9b381d08efd1e40c698
Amelya Kartika <126840619+AmelyaKartika@users.noreply.github.com>
2024-01-14 13:52:55 +0700

    calculator_test_2201082002
    Project Flutter

---
+++ calculator_test_2201082002/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:calculator_test_2201082002/second_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyCalculatorApp());
+}
+
+class MyCalculatorApp extends StatefulWidget {
+  const MyCalculatorApp({super.key});
+
+  @override
+  State<MyCalculatorApp> createState() => _MyCalculatorAppState();
+}
+
+class _MyCalculatorAppState extends State<MyCalculatorApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("Calculator Test"),
+          ),
+        ),
+        body: Center(
+          child: Builder(
+            builder: (context) {
+              return ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return MyWidget();
+                      },
+                    ),
+                  );
+                },
+                child: Text("OKE!"),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
