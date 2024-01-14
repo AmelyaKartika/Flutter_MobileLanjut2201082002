b0fc952df6e032846d686bcfc7068303c328aec3
Amelya Kartika <126840619+AmelyaKartika@users.noreply.github.com>
2024-01-14 13:19:58 +0700

    model_2201082002
    Project Flutter

---
+++ model_2201082002/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:model_2201082002/page/home_page.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
